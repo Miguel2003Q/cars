bf3a7e652d6eb429b67f5368f1c8343986f6fe64
MIGUEL2003Q <miguelmorales.10923@gmail.com>
2025-09-03 22:40:14 -0500

    Login correcto con descripcion de token

--- src/main/java/com/cars/cars/controller/AuthController.java
+++ src/main/java/com/cars/cars/controller/AuthController.java
@@ -5,6 +5,9 @@ import com.cars.cars.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -14,12 +17,36 @@ public class AuthController {
     private final AuthService authService;
     
     @PostMapping("/register")
-    public String register(@RequestBody User user) {
-        return authService.register(user);
+    public Map<String, Object> register(@RequestBody User user) {
+        String token = authService.register(user);
+        User savedUser = authService.getUserByUsername(user.getUsername());
+        
+        Map<String, Object> response = new HashMap<>();
+        response.put("token", token);
+        response.put("user", Map.of(
+            "id", savedUser.getId(),
+            "username", savedUser.getUsername(),
+            "email", savedUser.getEmail(),
+            "firstName", savedUser.getFirstName(),
+            "lastName", savedUser.getLastName()
+        ));
+        return response;
     }
     
     @PostMapping("/login")
-    public String login(@RequestBody User user) {
-        return authService.login(user.getUsername(), user.getPassword());
+    public Map<String, Object> login(@RequestBody User user) {
+        String token = authService.login(user.getUsername(), user.getPassword());
+        User loggedUser = authService.getUserByUsername(user.getUsername());
+        
+        Map<String, Object> response = new HashMap<>();
+        response.put("token", token);
+        response.put("user", Map.of(
+            "id", loggedUser.getId(),
+            "username", loggedUser.getUsername(),
+            "email", loggedUser.getEmail(),
+            "firstName", loggedUser.getFirstName(),
+            "lastName", loggedUser.getLastName()
+        ));
+        return response;
     }
 }
